8f935bdf7f11abfa24b5fbf8f0e25bedc8b33774
ekastn <118880703+ekastn@users.noreply.github.com>
2025-06-17 01:00:19 +0700

    Initial react + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <main className="flex items-center justify-center w-screen h-screen">
+            <h1 className="text-3xl font-bold uppercase">SheMaps</h1>
+      </main>
+  )
+}
+
+export default App
