2e322c5bde2ecfc26430cabf54d767f1457a7f81
ekastn <118880703+ekastn@users.noreply.github.com>
2025-06-18 00:33:17 +0700

    feat: add shadcn component library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { SMMap } from "./components/maps/SMMap.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 function App() {
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
     return (
-        <APIProvider apiKey={apiKey}>
-            <main className="w-screen h-screen">
-                <SMMap center={{ lat: 37.7749, lng: -122.4194 }} zoom={10} />
-            </main>
-        </APIProvider>
+        // <APIProvider apiKey={apiKey}>
+        <main className="w-screen h-screen">
+            <Button>Shemaps</Button>
+            {/* <SMMap center={{ lat: 37.7749, lng: -122.4194 }} zoom={10} /> */}
+        </main>
+        // {/* </APIProvider> */}
     );
 }
 
